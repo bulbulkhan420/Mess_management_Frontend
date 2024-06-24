bba29f1d24da044b1e66393bb3887b95a5159728
Sha Alam Bulbul <bulbulkhan420420@gmail.com>
2024-06-25 01:06:16 +0600

    New files

--- src/Url.jsx
+++ src/Url.jsx
@@ -1,3 +1,4 @@
-export let url="http://localhost:3005"
+export let url="https://mess-management-backend-3.onrender.com"
 
 //"https://mess-management-backend-3.onrender.com"
+//"http://localhost:3005"
